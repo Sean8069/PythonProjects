43d04d965f7e23fbcce5cca466c1961df7c3d677
Sean8069 <seanlau8069@gmail.com>
2021-05-25 15:34:44 +0800

    added MAC_Changer function

--- MAC_Address_Changer/MAC_Address_Changer.py
+++ MAC_Address_Changer/MAC_Address_Changer.py
@@ -1,1 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/env/python3
+
+import subprocess
+
+def MAC_Changer():
+
+    '''
+    This Function is to Change MAC Address of an Interface   
+    '''
+
+    subprocess.run(['ifconfig', 'eth0', 'down'])
+    subprocess.run(['ifconfig', 'eth0', 'hw', 'ether', '00:11:22:33:44:55'])
+    subprocess.run(['ifconfig', 'eth0', 'up'])
+
+    print(f'[+] MAC Address has successfully changed to 00:11:22:33:44:55')
+
+
+MAC_Changer()
