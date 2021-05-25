16a7bd786351d92ef936f4d5bbf022395265f5c5
Sean8069 <seanlau8069@gmail.com>
2021-05-25 16:00:00 +0800

    added validation for user_input function

--- MAC_Address_Changer/MAC_Address_Changer.py
+++ MAC_Address_Changer/MAC_Address_Changer.py
@@ -9,11 +9,17 @@ def User_Input():
     This Function provides option for user input
     '''
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--interface', dest='interface', help='interface to change MAC address')
-    parser.add_argument('-m', '--mac_addr', dest='new_mac', help='new MAC address')
+    parser = argparse.ArgumentParser(description='This is a MAC Address changer program.')
+    parser.add_argument('-i', '--interface', dest='interface', help='interface to change MAC address', metavar='')
+    parser.add_argument('-m', '--mac_addr', dest='new_mac', help='new MAC address', metavar='')
     args = parser.parse_args()
-    return args
+
+    if not args.interface:
+        parser.error('[-] Error Interface: Please input a valid interface, refer --help for more information.')
+    elif not args.new_mac:
+        parser.error('[-] Error MAC: Please input a valid MAC address, refer --help for more information.')
+    else:
+        return args
 
 
 def MAC_Changer(interface, new_mac):
